825e0149c8df570ebaa6598d89c7000f411b6643
Nathan Ferdinando <nferdica@outlook.com>
2024-08-06 17:27:23 -0300

    Create CRUD

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from "sequelize"
 import dataBaseConfig from "../config/database"
 import Aluno from "../models/aluno"
+import User from "../models/user"
 
-const models = [Aluno]; 
+const models = [Aluno, User];
 const conection =  new Sequelize(dataBaseConfig)
 
 models.forEach(model => model.init(conection))
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import userController from "../controllers/userController";
+const router = new Router();
+
+router.post("/", userController.store)
+router.get("/", userController.index)
+router.get("/:id", userController.show)
+router.put("/:id", userController.update)
+router.delete("/:id", userController.delete)
+
+
+export default router;
+
+/*
+Index -> Lista todos os usuários -> GET
+Store/Create -> Cria um novo usuário -> POST
+Delete -> Apaga um usuário -> DELETE
+Show -> Mostra um usuário -> GET
+Update -> Atualiza um usuário -> PATCH ou PUT
+*/
