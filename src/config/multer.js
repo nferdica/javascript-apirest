3dbe695289f460e42bad73741b245545ebc669ca
Nathan Ferdinando <nferdica@outlook.com>
2024-08-08 17:50:17 -0300

    feat: Adding handling file loading

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,9 +1,15 @@
-import multer from "multer";
+import multer, { MulterError } from "multer";
 import path, {extname, resolve} from 'path';
 
 const alt = () => Math.floor(Math.random() * 10000 + 10000)
 
 export default {
+  fileFilter: (req, file, cb) => {
+   if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG!'))
+    }
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'))
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,11 +1,7 @@
 import { Router } from "express";
-import multer from "multer";
 import fotoController from "../controllers/fotoController";
-import multerConfig from "../config/multer"
 const router = new Router();
 
-const upload = multer(multerConfig)
-
-router.post("/", upload.single('archive'), fotoController.store)
+router.post("/", fotoController.store)
 
 export default router;
