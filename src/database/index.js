6bf60686c7986f5e4387481c944b66412fdd4a32
Nathan Ferdinando <nferdica@outlook.com>
2024-08-08 19:40:02 -0300

    feat: Creating route to save photos in the database

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,10 @@ import Sequelize from "sequelize"
 import dataBaseConfig from "../config/database"
 import Aluno from "../models/aluno"
 import User from "../models/user"
+import Foto from "../models/foto"
 
-const models = [Aluno, User];
+const models = [Aluno, User, Foto];
 const conection =  new Sequelize(dataBaseConfig)
 
 models.forEach(model => model.init(conection))
+models.forEach(model => model.associate && model.associate(conection.models));
--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import fotoController from "../controllers/fotoController";
+import loginRequired from "../middlewares/loginRequired"
 const router = new Router();
 
-router.post("/", fotoController.store)
+router.post("/", loginRequired, fotoController.store)
 
 export default router;
